d57af81546665cb3ce6a170e1f2ee5820307e9df
Vistar12 <ST10453490@rcconnect.edu.za>
2025-04-04 01:56:23 +0200

    in this class its where i was doing the logo

---
+++ MYPOEPT1/Ascii_Logo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace MYPOEPT1
+{
+    internal class Ascii_Logo
+    {
+        public Ascii_Logo()
+        {
+            string logo_path = AppDomain.CurrentDomain.BaseDirectory;
+
+
+            string new_path = logo_path.Replace("bin\\Debug\\", "");
+
+
+            string full_location = Path.Combine(new_path, "CYBERSECURITY.png");
+
+            // Checks if the file exists
+            if (!File.Exists(full_location))
+            {
+                Console.WriteLine("Error: Image file not found at " + full_location);
+                return;
+            }
+
+            //  For Loading and resize the image
+            Bitmap image = new Bitmap(full_location);
+            image = new Bitmap(image, new Size(70, 50));
+
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+
+
+            for (int height = 0; height < image.Height; height++)
+            {
+                for (int width = 0; width < image.Width; width++)
+                {
+                    Color Pixel_Color = image.GetPixel(width, height);
+                    int white = (Pixel_Color.R + Pixel_Color.G + Pixel_Color.B) / 3;
+                    char asciiChar = white > 200 ? '.' : white > 150 ? '*' : white > 100 ? 'o' : white > 50 ? '#' : '@';
+                    Console.Write(asciiChar);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+    }
+}
+        
